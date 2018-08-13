ea38ee1d4ae4999fdd2e1cc6811ffd1d0ab0b995
sahil-athrij <sahilathrij@gmail.com>
2018-08-14 04:05:19 +0530

    makeaton
    i hope i didnt srew up

--- make-a-ton/js/script.js
+++ make-a-ton/js/script.js
@@ -1,6 +1,16 @@
-$(document).ready(function(){
-    $('.frame').click(function(){
-        $('.top').addClass('open');
-        $('.message').addClass('pull');
-    })
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
 });
+
+
+
+
+
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.12),
+        zoom: 10,
+        map:map
+    }
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
